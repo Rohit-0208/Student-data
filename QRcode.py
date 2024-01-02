0aba1d7a8c97210570e1b39b93e6fb4a6a3e51e8
Rohit-0208 <enugalarohithreddy08@gmail.com>
2024-01-02 21:51:59 +0530

    Qrcode

---
+++ QRcode.py
@@ -0,0 +1,3 @@
+import qrcode as q
+img = q.make("Greeshu you are the best thing happend to me in my life.. My love towards you increasing day by day..you don't know how much i love , care and think about you..Hearing your smile makes my problems fade awayy...Mana madhya ee bond life long undali idaram okarni okaram support chesukuntu dreams full fill chesukontu lyf ni njoy cheyali ...Love you babe >>>>>>3000..")
+img.save("Greeshu.png")
